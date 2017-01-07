0ba4cf01eddce506cb4100ffbef9623b5df41a70
Japnit Kaur Ahuja <japnit.ahuja@gmail.com>
2017-01-07 07:03:55 +0530

    BST

---
+++ C++/Data Structures/BST/BST/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  BST
+//
+//  Created by Japnit Kaur Ahuja on 07/01/17.
+//  Copyright © 2017 Japnit Kaur Ahuja. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    
+};
+
+Node *root = NULL;
+
+void insert(int x)
+{
+    
+}
+
+
+int main()
+{
+    insert (8);
+    insert (9);
+    insert (5);
+    insert (6);
+    insert (15);
+    return 0;
+}
